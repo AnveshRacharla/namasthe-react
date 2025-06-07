035fe9414d4dcc61d825e724d41f4ae6f06968d0
AnveshRacharla <148354342+AnveshRacharla@users.noreply.github.com>
2025-06-07 22:17:12 +0530

    Updates

---
+++ Grocery.ac022571.js
@@ -0,0 +1,2 @@
+!function(){var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire164e;(0,e.register)("dhpq2",function(n,o){Object.defineProperty(n.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(n.exports,"default",{get:function(){return t},set:void 0,enumerable:!0,configurable:!0});var r=e("cc6u1"),t=function(){return(0,r.jsx)("h1",{children:"grocery store"})}})}();
+//# sourceMappingURL=Grocery.ac022571.js.map
---
+++ NAMASTE-REACT.7c0ccee6.js
@@ -0,0 +1,12 @@
+// import React from "react";
+const heading = React.createElement("h1", {
+    id: "title"
+}, "Namasthe Every1");
+// const heading2=React.createElement("h2",{},"Namasthe Every2");
+// const container=React.createElement("div",{},[heading,heading2]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading); // document.innerHTML=
+ // const head=document.getElementById("root");
+ // head.innerHTML="<h1>hi guych</h1>"
+
+//# sourceMappingURL=NAMASTE-REACT.7c0ccee6.js.map
